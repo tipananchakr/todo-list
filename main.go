7026d19c0439afefd3474e475648bd76d66572b8
tipananchakr <tipanan.chak@gmail.com>
2026-05-02 19:42:51 +0700

    ✨ CRUD {todo feature}

--- main.go
+++ main.go
@@ -1,14 +1,82 @@
 package main
 
 import (
+	"fmt"
 	"log"
 
 	"github.com/gofiber/fiber/v2"
 )
 
+type Todo struct {
+	ID        int    `json:"id"`
+	Completed bool   `json:"completed"`
+	Body      string `json:"body"`
+}
+
 func main() {
 	app := fiber.New()
 
-	log.Fatal(app.Listen(":3000"))
+	todos := []Todo{}
 
+	app.Get("/api/todos", func(c *fiber.Ctx) error {
+		return c.JSON(todos)
+	})
+
+	// create a new todo
+	app.Post("/api/todos", func(c *fiber.Ctx) error {
+		todo := &Todo{}
+
+		if err := c.BodyParser(todo); err != nil {
+			return err
+		}
+
+		if todo.Body == "" {
+			return c.Status(400).JSON(fiber.Map{
+				"error": "todo body is required",
+			})
+		}
+
+		todo.ID = len(todos) + 1
+		todos = append(todos, *todo)
+
+		return c.Status(201).JSON(todo)
+	})
+
+	// update a todo by id
+	app.Patch("/api/todos/:id", func(c *fiber.Ctx) error {
+		id := c.Params("id")
+
+		for i, todo := range todos {
+			if fmt.Sprint(todo.ID) == id {
+				todos[i].Completed = true
+				return c.Status(200).JSON(todos[i])
+
+			}
+		}
+
+		return c.Status(404).JSON(fiber.Map{
+			"error": "todo not found",
+		})
+	})
+
+	// delete a todo by id
+	app.Delete("/api/todos/:id", func(c *fiber.Ctx) error {
+		id := c.Params("id")
+
+		for i, todo := range todos {
+			if fmt.Sprint(todo.ID) == id {
+				todos = append(todos[:i], todos[i+1:]...)
+
+				return c.Status(200).JSON(fiber.Map{
+					"message": fmt.Sprintf("todo id: %d deleted", todo.ID),
+				})
+
+			}
+		}
+		return c.Status(404).JSON(fiber.Map{
+			"error": "todo not found",
+		})
+	})
+
+	log.Fatal(app.Listen(":3000"))
 }
